a45291c0855d067b7f61776b7b26d5ffd403f8af
MaxTarn <112612203+MaxTarn@users.noreply.github.com>
2024-08-21 10:52:23 +0200

    Added Models for Database, in the Shared project

---
+++ Models/MetaTagModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared;
+
+public class MetaTagModel
+{
+    [Key]
+    public int MetaTagId { get; set; }
+
+    public QuestionModel? Question { get; set; }
+    public int QuestionId { get; set; }
+    public string MetaName { get; set; } = null!;
+
+}
+
---
+++ Models/QuestionModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared;
+
+public class QuestionModel
+{
+    [Key]
+    public int QuestionId { get; set; }
+
+
+    public CategoryModel? Category { get; set; }
+
+    public int CategoryId { get; set; }
+
+
+    public required string CategoryName { get; set; } = null!;
+
+
+}

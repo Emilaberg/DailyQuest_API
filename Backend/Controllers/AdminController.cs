2c3d8a2900fd106583d343a7f5c83e818fa819ce
MaxTarn <112612203+MaxTarn@users.noreply.github.com>
2024-09-06 12:17:31 +0200

    Added Admin EndPoint

---
+++ Backend/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        PassKeyVerifier _passKeyVerifier;
+        public AdminController(PassKeyVerifier passKeyVerifier)
+        {
+            _passKeyVerifier = passKeyVerifier;
+        }
+
+
+        [HttpGet("{adminPassKey}")]
+        public async Task<IActionResult> GetAdmin(string? adminPassKey)
+        {
+            if (!_passKeyVerifier.RequestIsAdmin(HttpContext)) { return Unauthorized(); }
+            return Ok("You are an admin");
+        }
+    }
+}

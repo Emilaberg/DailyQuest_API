20974cbe02bab8ead38df4c2ff305a2aa6b50b35
MaxTarn <112612203+MaxTarn@users.noreply.github.com>
2024-09-11 15:04:48 +0200

    added migration to reflect the removal of required in the TicketModel

---
+++ Backend/Migrations/20240911125041_fixed TicketModel.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixedTicketModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 1,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 50, 41, 172, DateTimeKind.Local).AddTicks(9637));
+
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 2,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 50, 41, 172, DateTimeKind.Local).AddTicks(9678));
+
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 3,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 50, 41, 172, DateTimeKind.Local).AddTicks(9680));
+
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 4,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 50, 41, 172, DateTimeKind.Local).AddTicks(9681));
+
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 5,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 50, 41, 172, DateTimeKind.Local).AddTicks(9682));
+
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 6,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 50, 41, 172, DateTimeKind.Local).AddTicks(9683));
+
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 7,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 50, 41, 172, DateTimeKind.Local).AddTicks(9685));
+
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 8,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 50, 41, 172, DateTimeKind.Local).AddTicks(9686));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 1,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 21, 34, 549, DateTimeKind.Local).AddTicks(3398));
+
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 2,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 21, 34, 549, DateTimeKind.Local).AddTicks(3441));
+
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 3,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 21, 34, 549, DateTimeKind.Local).AddTicks(3443));
+
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 4,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 21, 34, 549, DateTimeKind.Local).AddTicks(3445));
+
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 5,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 21, 34, 549, DateTimeKind.Local).AddTicks(3447));
+
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 6,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 21, 34, 549, DateTimeKind.Local).AddTicks(3449));
+
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 7,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 21, 34, 549, DateTimeKind.Local).AddTicks(3451));
+
+            migrationBuilder.UpdateData(
+                table: "Emails",
+                keyColumn: "EmailId",
+                keyValue: 8,
+                column: "DateLastSent",
+                value: new DateTime(2024, 9, 11, 14, 21, 34, 549, DateTimeKind.Local).AddTicks(3453));
+        }
+    }
+}

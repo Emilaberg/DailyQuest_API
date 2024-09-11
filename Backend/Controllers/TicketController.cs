9ee82a4b879efefca22979db4587bbf148b71a98
MaxTarn <112612203+MaxTarn@users.noreply.github.com>
2024-09-11 15:03:48 +0200

    Fixed the Post EndPoint and added Put , Delete , and GetAll and GetById endpoints

--- Backend/Controllers/TicketController.cs
+++ Backend/Controllers/TicketController.cs
@@ -9,17 +9,63 @@ namespace Backend.Controllers;
 public class TicketController : ControllerBase
 {
     private readonly ITicketRepository _ticketRepository;
-    public TicketController(ITicketRepository ticketRepository)
+    private readonly PassKeyVerifier _passKeyVerifier;
+    public TicketController(ITicketRepository ticketRepository, PassKeyVerifier passKeyVerifier)
     {
         _ticketRepository = ticketRepository;
+        _passKeyVerifier = passKeyVerifier;
     }
 
-    [HttpPost]
-    public async Task<IActionResult> PostTicket(TicketModel ticket)
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TicketModel>>> GetTickets()
     {
+        var tickets = await _ticketRepository.GetAllAsync();
+        return Ok(tickets);
+    }
+
+    [HttpGet("{ticketId}")]
+    public async Task<ActionResult<TicketModel?>> GetTicket(int ticketId)
+    {
+        return Ok(await _ticketRepository.GetByIdAsync(ticketId));
+    }
+
+
+    [HttpPost("{adminPssKey}")]
+    public async Task<IActionResult> PostTicket(string? adminPassKey, string email, string subject, string message)
+    {
+        if (!_passKeyVerifier.RequestIsAdmin(HttpContext)) { return Unauthorized(); }
+        TicketModel ticket = new TicketModel
+        {
+            Email = email,
+            Subject = subject,
+            Message = message,
+            TimeStamp = DateTime.Now
+        };
         await _ticketRepository.AddAsync(ticket);
         return Ok("Ticket saved successfully.");
     }
 
+    [HttpPut("{adminPassKey}")]
+    public async Task<IActionResult> PutTicket(string? adminPassKey, string email, string subject, string message)
+    {
+        if (!_passKeyVerifier.RequestIsAdmin(HttpContext)) { return Unauthorized(); }
+        TicketModel ticket = new TicketModel
+        {
+            Email = email,
+            Subject = subject,
+            Message = message,
+            TimeStamp = DateTime.Now
+        };
+        await _ticketRepository.UpdateAsync(ticket);
+        return Ok("Ticket updated successfully.");
+    }
+
+    [HttpDelete("{adminPassKey}")]
+    public async Task<IActionResult> DeleteTicket(string? adminPassKey, int ticketId)
+    {
+        if (!_passKeyVerifier.RequestIsAdmin(HttpContext)) { return Unauthorized(); }
+        await _ticketRepository.DeleteAsync(ticketId);
+        return Ok("Ticket deleted successfully.");
+    }
 }
 
